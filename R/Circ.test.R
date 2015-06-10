86505305a7202e725c34ccabfc85a94df7b78a76
Jun Cheng <s6juncheng@gmail.com>
2015-06-10 11:13:11 +0200

    modify Circ.test.R

--- R/Circ.test.R
+++ R/Circ.test.R
@@ -4,6 +4,7 @@
 #' Test the independent variation of circRNAs in relevant to their host genes. 
 #' @param Circ CircRNACount file. A file of circRNA read count table. First three columns are circRNA coordinates, and followed by columns for circRNA read counts, each sample per column. 
 #' @param Linear LinearCount file. A file of circRNA host gene expression count table. Same configuration as CircRNACount file.
+#' @param (Optional) CircCoordinates BED format circRNA coordinates file. 
 #' @param group A vector of group indicators.
 #' @param alpha p value cut off. Defaul 0.05.
 #' @param plotsig If 'TRUE', significantly host-independently regulated circRNAs will be ploted.
@@ -16,7 +17,7 @@
 #' Circ.ratioplot(Circ,Linear,Coordinates,plotrow=rownames(test$sig.dat)[1],groupindicator1=c(rep('1days',6),rep('4days',6),rep('20days',6)),groupindicator2=rep(c(rep('Female',4),rep('Male',2)),3),lab_legend='Ages')
 #' @export Circ.test
 
-Circ.test <- function(Circ=Circ,Linear=Linear,group,alpha=0.05,plotsig=T){
+Circ.test <- function(Circ,Linear,CircCoordinates=None,group,alpha=0.05,plotsig=T){
   # Requre packge
   require(aod)
   
@@ -32,7 +33,7 @@ Circ.test <- function(Circ=Circ,Linear=Linear,group,alpha=0.05,plotsig=T){
   if ( length(group) != ncol(Circ)-3 ){
     stop("length of 'group' must be equal to the number of samples of 'Circ' and 'Linear'. ")
   }
-  group = factor(group)
+  group <- factor(group)
 
   ## test
   # constract test matrix for each circRNA  
@@ -62,15 +63,21 @@ Circ.test <- function(Circ=Circ,Linear=Linear,group,alpha=0.05,plotsig=T){
     a <- anova(fitNull,fitAlt)
     p.value <- a@anova.table[,11][2]
     print(p.value)
-    p.val = c( p.val, p.value ) 
+    p.val <- c( p.val, p.value ) 
   }
   
   p.adj <- p.adjust(p.val,n=sum(!is.na(p.val)),'BH')
   # select significant ones
-  sig_dat=Circ[p.adj<=alpha  & !is.na(p.adj),]
-  sig_p=p.adj[p.adj<=alpha  & !is.na(p.adj)]
+  sig_dat <- Circ[p.adj<=alpha  & !is.na(p.adj),]
+  sig_p <- p.adj[p.adj<=alpha  & !is.na(p.adj)]
   # sort
-  sig_dat = sig_dat[order(sig_p),]
-  sig_p = sort(sig_p)
-  return(list(p.val=p.val,p.adj=p.adj,sig.dat=sig_dat,sig_p=sig_p))
+  sig_dat <- sig_dat[order(sig_p),]
+  sig_p <- sort(sig_p)
+  # A summary table
+  if (missing(CircCoordinates)){
+    summary_table <- cbind(sig_dat[,c(1:3)],sig_p)
+  }else{
+    summary_table <- cbind(CircCoordinates[rownames(sig_dat),],sig_p)
+  }
+  return(list(summary_table=summary_table,sig.dat=sig_dat,p.val=p.val,p.adj=p.adj,sig_p=sig_p))
 }
